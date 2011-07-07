11ce572ec5d782b859a997869f11a96eaf2475a8
Tak Tran <contact@tutaktran.com>
2011-07-07 16:03:00 +0100

    Corrected array init and added uiview/uiviewcontroller.

--- objective-c.m
+++ objective-c.m
@@ -120,13 +120,31 @@ NSArray *arrayWithObject = [NSArray arrayWithObject:@"one"];
 NSArray *arrayWithObjects = [NSArray arrayWithObjects:@"one", @"two", @"three", nil];
 
 // Need to memory manage
-NSArray *emptyArray = [NSArray initWithArray:someArray];
+NSArray *emptyArray = [[NSArray alloc] initWithArray:someArray];
 // ...
 
 
 NSMutableArray *mutableArray = [NSMutableArray initWithCapacity:5];
 [mutableArray insertObject:@"one" atIndex:0];
 [mutableArray addObject:@"one"];
+
+#pragma mark -
+#pragma mark === UIView/UIViewController ===
+#pragma mark
+
+UIViewController *vc = [[UIViewController alloc] initWithNibName:@"nibName" bundle:nil];
+vc.view; // Main view
+vc.title; // Title used in navigation controllers
+[vc presentModalViewController:viewController animated:YES];
+[vc dismissModalViewControllerAnimated:YES];
+[vc.view addSubview:view];
+view.frame = CGRectMake(x, y, view.frame.size.width, view.frame.size.height);
+
+#pragma mark -
+#pragma mark === Random numbers ===
+#pragma mark
+
+int ri = arc4random() % maxNum;
 
 #pragma mark -
 #pragma mark === Debugging ===
